86e47d75bb06e457b47d5718d117cf1f15b0f484
MicroGrey <wupeiqin@sjtu.edu.cn>
2025-12-15 20:46:02 +0800

    feat: new arch with dynamic block_num, PixelAttention and PReLU

--- basicsr/archs/bsrn_arch.py
+++ basicsr/archs/bsrn_arch.py
@@ -14,6 +14,18 @@ import basicsr.archs.Upsamplers as Upsamplers
 from basicsr.utils.registry import ARCH_REGISTRY
 
 
+class PixelAttention(nn.Module):
+    def __init__(self, num_feat):
+        super(PixelAttention, self).__init__()
+        self.conv1 = nn.Conv2d(num_feat, num_feat, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # 1x1 卷积生成注意力图 -> Sigmoid -> 乘回原图
+        attn = self.conv1(x)
+        attn = self.sigmoid(attn)
+        return x * attn
+
 class DepthWiseConv(nn.Module):
     def __init__(self, in_ch, out_ch, kernel_size=3, stride=1, padding=1,
                  dilation=1, bias=True, padding_mode="zeros", with_norm=False, bn_kwargs=None):
@@ -216,14 +228,14 @@ class ESA(nn.Module):
         self.conv3_ = conv(f, f, kernel_size=3, **BSConvS_kwargs)
         self.conv4 = nn.Conv2d(f, num_feat, 1)
         self.sigmoid = nn.Sigmoid()
-        self.GELU = nn.GELU()
+        self.PReLU = nn.PReLU()
 
     def forward(self, input):
         c1_ = (self.conv1(input))
         c1 = self.conv2(c1_)
         v_max = self.maxPooling(c1)
-        v_range = self.GELU(self.conv_max(v_max))
-        c3 = self.GELU(self.conv3(v_range))
+        v_range = self.PReLU(self.conv_max(v_max))
+        c3 = self.PReLU(self.conv3(v_range))
         c3 = self.conv3_(c3)
         c3 = F.interpolate(c3, (input.size(2), input.size(3)), mode='bilinear', align_corners=False)
         cf = self.conv_f(c1_)
@@ -251,11 +263,11 @@ class ESDB(nn.Module):
         self.c3_r = conv(self.remaining_channels, self.rc, kernel_size=3, **kwargs)
 
         self.c4 = conv(self.remaining_channels, self.dc, kernel_size=3, **kwargs)
-        self.act = nn.GELU()
+        self.act = nn.PReLU(num_parameters=1) 
 
         self.c5 = nn.Conv2d(self.dc * 4, in_channels, 1)
         self.esa = ESA(in_channels, conv)
-        self.cca = CCALayer(in_channels)
+        self.pa = PixelAttention(in_channels)
 
     def forward(self, input):
 
@@ -276,7 +288,7 @@ class ESDB(nn.Module):
         out = torch.cat([distilled_c1, distilled_c2, distilled_c3, r_c4], dim=1)
         out = self.c5(out)
         out_fused = self.esa(out)
-        out_fused = self.cca(out_fused)
+        out_fused = self.pa(out_fused)
         return out_fused + input
 
 
@@ -306,17 +318,12 @@ class BSRN(nn.Module):
             self.conv = nn.Conv2d
         self.fea_conv = self.conv(num_in_ch * 4, num_feat, kernel_size=3, **kwargs)
 
-        self.B1 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
-        self.B2 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
-        self.B3 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
-        self.B4 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
-        self.B5 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
-        self.B6 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
-        self.B7 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
-        self.B8 = ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p)
+        self.blocks = nn.ModuleList()
+        for _ in range(num_block):
+            self.blocks.append(ESDB(in_channels=num_feat, out_channels=num_feat, conv=self.conv, p=p))
 
         self.c1 = nn.Conv2d(num_feat * num_block, num_feat, 1)
-        self.GELU = nn.GELU()
+        self.PReLU = nn.PReLU()
 
         self.c2 = self.conv(num_feat, num_feat, kernel_size=3, **kwargs)
 
@@ -335,18 +342,15 @@ class BSRN(nn.Module):
     def forward(self, input):
         input = torch.cat([input, input, input, input], dim=1)
         out_fea = self.fea_conv(input)
-        out_B1 = self.B1(out_fea)
-        out_B2 = self.B2(out_B1)
-        out_B3 = self.B3(out_B2)
-        out_B4 = self.B4(out_B3)
-        out_B5 = self.B5(out_B4)
-        out_B6 = self.B6(out_B5)
-        out_B7 = self.B7(out_B6)
-        out_B8 = self.B8(out_B7)
+        distilled_features = []
+        x = out_fea
+        for block in self.blocks:
+            x = block(x)
+            distilled_features.append(x)
 
-        trunk = torch.cat([out_B1, out_B2, out_B3, out_B4, out_B5, out_B6, out_B7, out_B8], dim=1)
+        trunk = torch.cat(distilled_features, dim=1)
         out_B = self.c1(trunk)
-        out_B = self.GELU(out_B)
+        out_B = self.PReLU(out_B)
 
         out_lr = self.c2(out_B) + out_fea
 
